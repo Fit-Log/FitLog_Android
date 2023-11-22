95edd51d545be3d21c12b7cf75e333b71d9c2847
diddbsgh <yangyoonho0710@naver.com>
2023-11-22 23:13:05 +0900

    📝 :: 홈 화면 기능 구현

--- app/src/main/java/app/yunho/fitlog_android/MainActivity.kt
+++ app/src/main/java/app/yunho/fitlog_android/MainActivity.kt
@@ -1,11 +1,38 @@
 package app.yunho.fitlog_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.icu.util.LocaleData
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.yunho.fitlog_android.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivPlaylist.setOnClickListener {
+            val youtubeLink = "https://www.youtube.com/watch?v=JsOOis4bBFg"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+
+        val currentDate = getCurrentDate()
+
+        binding.tvDate.text = currentDate
+    }
+
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val currentDate = Calendar.getInstance().time
+        return dateFormat.format(currentDate)
     }
 }
