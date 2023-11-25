c0116be14afe0e4f9b17ed240bf4243cd1bf5149
diddbsgh <yangyoonho0710@naver.com>
2023-11-26 01:23:36 +0900

    📝 :: 로그인

--- app/src/main/java/app/yunho/fitlog_android/LoginStartActivity.kt
+++ app/src/main/java/app/yunho/fitlog_android/LoginStartActivity.kt
@@ -1,11 +1,22 @@
 package app.yunho.fitlog_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.yunho.fitlog_android.databinding.ActivityLoginStartBinding
 
 class LoginStartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_start)
+        binding = ActivityLoginStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
