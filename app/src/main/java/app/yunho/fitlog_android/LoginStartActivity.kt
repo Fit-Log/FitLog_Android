8c1c7dac6206d02b159c111f34ffc983fb0f7d6f
diddbsgh <yangyoonho0710@naver.com>
2023-11-26 01:09:38 +0900

    📝 :: 로그인(시작화면)

---
+++ app/src/main/java/app/yunho/fitlog_android/LoginStartActivity.kt
@@ -0,0 +1,11 @@
+package app.yunho.fitlog_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_start)
+    }
+}
