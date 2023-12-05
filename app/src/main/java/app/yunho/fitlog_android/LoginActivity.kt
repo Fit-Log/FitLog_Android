2612654b227f266f7466b5f36c354a10826e2821
diddbsgh <yangyoonho0710@naver.com>
2023-12-05 19:14:49 +0900

    📝 :: 로그인

--- app/src/main/java/app/yunho/fitlog_android/LoginActivity.kt
+++ app/src/main/java/app/yunho/fitlog_android/LoginActivity.kt
@@ -1,11 +1,33 @@
 package app.yunho.fitlog_android
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private val etId: EditText by lazy { findViewById(R.id.et_id) }
+    private val etPassword: EditText by lazy { findViewById(R.id.et_password) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etId.setOnClickListener {
+            setEditTextBackground(etId, R.drawable.btn_selected)
+            setEditTextBackground(etPassword, R.drawable.btn_unselected)
+        }
+
+        etPassword.setOnClickListener {
+            setEditTextBackground(etPassword, R.drawable.btn_selected)
+            setEditTextBackground(etId, R.drawable.btn_unselected)
+        }
+    }
+
+    private fun setEditTextBackground(editText: EditText, drawableResId: Int) {
+        val drawable: Drawable? = ContextCompat.getDrawable(this, drawableResId)
+        editText.background = drawable
     }
 }
